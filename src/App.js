cd56886b9d3e7cb65632bf27a6572fcb4654a54b
emilhajizade <emilhacizade44@mail.ru>
2022-09-20 08:02:09 +0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import AddTodo from './components/AddTodo';
 import ListTodo from './components/ListTodo';
 import { createContext, useState } from 'react';
+import CompletedTodo from './components/CompletedTodo';
 
 export const TodoContext = createContext()
 
@@ -17,8 +18,9 @@ function App() {
       <TodoContext.Provider value={first} >
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<AddTodo/>}/>
-          <Route path='/list' element={<ListTodo/>}/>
+          <Route path='/' element={<ListTodo/>}/>
+          <Route path='/add' element={<AddTodo/>}/>
+          <Route path='/completed' element={<CompletedTodo/>}/>
         </Routes>
       </BrowserRouter>
       </TodoContext.Provider >
---
+++ src/components/CompletedTodo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../layouts/Navbar'
+
+function CompletedTodo() {
+  return (
+    <div>
+        <Navbar/>
+        CompletedTodo</div>
+  )
+}
+
+export default CompletedTodo
